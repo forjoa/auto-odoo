7ae103e81565f30d523f346bdb5c694487eb6340
joaquin <joaquin_trujillo@icloud.com>
2025-02-11 15:30:41 +0100

    build: manifest config

---
+++ addons/payment_reminder/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Payment Reminder',
+    'version': '1.0',
+    'summary': 'Automated payment reminders for invoices',
+    'author': 'Tu Nombre',
+    'category': 'Accounting',
+    'depends': ['account', 'mail'],
+    'data': [
+        'data/payment_reminder.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
